654135695b714646b62913f3c08d0168b10c795d
Serkuz90 <87276332+KurokoTetsuya64@users.noreply.github.com>
2022-01-22 17:34:18 +0400

    add figure builder 10% functionality

---
+++ Task 2/CUSTOM PAINT/CUSTOM PAINT/Classes/FigureBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUSTOM_PAINT.Classes
+{
+    internal class FigureBuilder
+    {
+        public Figure BuildFigure(FiguresType type) =>
+            type switch
+            {
+                FiguresType.Circle => CreateCircle(),
+                FiguresType.Ring => CreateRing(),
+                FiguresType.Rectangle => CreateRectangle(),
+                FiguresType.Square => CreateSquare(),
+                FiguresType.Triangle => CreateTriangle(),
+                FiguresType.Line => CreateLine(),
+                _ => throw new ArgumentOutOfRangeException(nameof(type)),
+            };
+    }
+}
--- Task 2/CUSTOM PAINT/CUSTOM PAINT/Program.cs
+++ Task 2/CUSTOM PAINT/CUSTOM PAINT/Program.cs
@@ -15,7 +15,34 @@ namespace CustomPaint
     {
         public static void Main(string[] args)
         {
-            public string ChooseAction = "Выберите действие";
+            int action = -1;
+            string str1 = "1: Добавить фигуру";
+            string str2 = "2: Вывести фигуры";
+            string str3 = "3: Очистить холст";
+            string str4 = "5: Выход";
+            string newL = Environment.NewLine;
+
+            while (action != 5)
+            {
+                Console.WriteLine("Введите число");
+                Console.WriteLine(str1 + newL + str2 + newL + str3 + newL + str4);
+                action = ReadAndParce();
+            }
+        }
+
+        static bool Check(int x) => x > 0;
+        static int ReadAndParce()
+        {
+            string N = Console.ReadLine() ?? string.Empty;
+
+            int number;
+
+            if (int.TryParse(N, out number) && Check(number)) return number;
+            else
+            {
+                Console.WriteLine("Введите значение заново");
+                return ReadAndParce();
+            }
         }
     }
 }
